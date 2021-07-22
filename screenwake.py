e5c1037cad6a8380377a0d63634ab7a6e2a377ba
Chris Wren <51368079+vader7071@users.noreply.github.com>
2021-07-22 18:37:25 -0500

    Update screenwake.py

--- screenwake.py
+++ screenwake.py
@@ -10,7 +10,24 @@ GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(14, GPIO.FALLING)
 
 from subprocess import run
-run('vcgencmd display_power 1', shell=True)
+run('vcgencmd display_power 1', shell=False)
+
+##### Time Delay to turn screen off #####
+# import time
+#
+# counter = 300 # 5 minutes
+# start = time.time()
+# while True:
+#    time.sleep(0.1)
+#
+#    if time.time() - start > 1:
+#        start = time.time()
+#        counter = counter - 1
+#
+#        if counter <= 0:
+#            run('vcgencmd display_power 0', shell=False)
+#            break
+#########################################
 
 # from "Howchoo"
 # subprocess.call(['shutdown', '-h', 'now'], shell=False)
